bd3ec43f94c891ddfc0cfee3445a31b12f5d95b5
belven <belven000@gmail.com>
2014-10-16 11:53:07 +0100

    Renamed manager and balanced the buffs

--- src/belven/mobs/listeners/MobListener.java
+++ src/belven/mobs/listeners/MobListener.java
@@ -11,66 +11,58 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import resources.EntityFunctions;
-
-import belven.mobs.MobManager;
+import belven.mobs.MobBuffManager;
 import belven.mobs.resources.functions;
 
-public class MobListener implements Listener
-{
-    private final MobManager plugin;
+public class MobListener implements Listener {
+	private final MobBuffManager plugin;
 
-    Random randomGenerator = new Random();
+	Random randomGenerator = new Random();
 
-    public MobListener(MobManager instance)
-    {
-        plugin = instance;
-    }
+	public MobListener(MobBuffManager instance) {
+		plugin = instance;
+	}
 
-    @EventHandler
-    public void onCreatureSpawnEvent(CreatureSpawnEvent event){
-    	
-        if (EntityFunctions.IsAMob(event.getEntityType()))
-        {
-            LivingEntity le = event.getEntity();
+	@EventHandler
+	public void onCreatureSpawnEvent(CreatureSpawnEvent event) {
 
-            PotionEffectType positiveEffect = getPositiveEffect(le);
-            int posAmplifier = functions.getPotionEffectMaxAmp(positiveEffect);
-            int posDuration = functions
-                    .getPotionEffectMaxDuration(positiveEffect);
+		if (EntityFunctions.IsAMob(event.getEntityType())) {
+			LivingEntity le = event.getEntity();
 
-            PotionEffectType negativeEffect = getNegativeEffect(le);
-            int negAmplifier = functions.getPotionEffectMaxAmp(positiveEffect);
-            int negDuration = functions
-                    .getPotionEffectMaxDuration(positiveEffect);
+			PotionEffectType positiveEffect = getPositiveEffect(le);
+			int posAmplifier = functions.getPotionEffectMaxAmp(positiveEffect);
+			int posDuration = functions
+					.getPotionEffectMaxDuration(positiveEffect);
 
-            PotionEffect positive = new PotionEffect(positiveEffect,
-                    posDuration, posAmplifier);
+			PotionEffectType negativeEffect = getNegativeEffect(le);
+			int negAmplifier = functions.getPotionEffectMaxAmp(positiveEffect);
+			int negDuration = functions
+					.getPotionEffectMaxDuration(positiveEffect);
 
-            PotionEffect negative = new PotionEffect(negativeEffect,
-                    negDuration, negAmplifier);
+			PotionEffect positive = new PotionEffect(positiveEffect,
+					posDuration, posAmplifier);
 
-            le.addPotionEffect(positive, true);
-            le.addPotionEffect(negative, true);
-        }
-    }
+			new PotionEffect(negativeEffect, negDuration, negAmplifier);
 
-    private PotionEffectType getPositiveEffect(LivingEntity le)
-    {
-        List<PotionEffectType> tempEffects = functions.positiveEffectsForMobs();
-        int ran = randomGenerator.nextInt(tempEffects.size());
-        return tempEffects.get(ran);
-    }
+			le.addPotionEffect(positive, true);
+			// le.addPotionEffect(negative, true);
+		}
+	}
 
-    private PotionEffectType getNegativeEffect(LivingEntity le)
-    {
-        List<PotionEffectType> tempEffects = functions.negativeEffectsForMobs();
-        int ran = randomGenerator.nextInt(tempEffects.size());
-        return tempEffects.get(ran);
-    }
+	private PotionEffectType getPositiveEffect(LivingEntity le) {
+		List<PotionEffectType> tempEffects = functions.positiveEffectsForMobs();
+		int ran = randomGenerator.nextInt(tempEffects.size());
+		return tempEffects.get(ran);
+	}
 
-    public MobManager getPlugin()
-    {
-        return plugin;
-    }
+	private PotionEffectType getNegativeEffect(LivingEntity le) {
+		List<PotionEffectType> tempEffects = functions.negativeEffectsForMobs();
+		int ran = randomGenerator.nextInt(tempEffects.size());
+		return tempEffects.get(ran);
+	}
+
+	public MobBuffManager getPlugin() {
+		return plugin;
+	}
 
 }
--- src/belven/mobs/resources/functions.java
+++ src/belven/mobs/resources/functions.java
@@ -7,186 +7,145 @@ import org.bukkit.potion.PotionEffectType;
 
 import resources.Functions;
 
-public class functions{
+public class functions {
+	final static int SECOND = 1;
+	final static int MINUTE = SECOND * 60;
+	final static int HOUR = MINUTE * 60;
+	final static int DAY = HOUR * 24;
 
-    
+	public static List<PotionEffectType> negativeEffects() {
+		List<PotionEffectType> tempEffects = new ArrayList<PotionEffectType>();
+		tempEffects.add(PotionEffectType.BLINDNESS);
+		tempEffects.add(PotionEffectType.CONFUSION);
+		tempEffects.add(PotionEffectType.HARM);
+		tempEffects.add(PotionEffectType.HUNGER);
+		tempEffects.add(PotionEffectType.POISON);
+		tempEffects.add(PotionEffectType.SLOW);
+		tempEffects.add(PotionEffectType.SLOW_DIGGING);
+		tempEffects.add(PotionEffectType.WEAKNESS);
+		tempEffects.add(PotionEffectType.WITHER);
+		return tempEffects;
+	}
 
-    public static List<PotionEffectType> negativeEffects()
-    {
-        List<PotionEffectType> tempEffects = new ArrayList<PotionEffectType>();
-        tempEffects.add(PotionEffectType.BLINDNESS);
-        tempEffects.add(PotionEffectType.CONFUSION);
-        tempEffects.add(PotionEffectType.HARM);
-        tempEffects.add(PotionEffectType.HUNGER);
-        tempEffects.add(PotionEffectType.POISON);
-        tempEffects.add(PotionEffectType.SLOW);
-        tempEffects.add(PotionEffectType.SLOW_DIGGING);
-        tempEffects.add(PotionEffectType.WEAKNESS);
-        tempEffects.add(PotionEffectType.WITHER);
-        return tempEffects;
-    }
+	public static List<PotionEffectType> undeadImmuneEffects() {
+		List<PotionEffectType> tempEffects = new ArrayList<PotionEffectType>();
+		tempEffects.add(PotionEffectType.REGENERATION);
+		tempEffects.add(PotionEffectType.POISON);
+		return tempEffects;
+	}
 
-    public static List<PotionEffectType> undeadImmuneEffects()
-    {
-        List<PotionEffectType> tempEffects = new ArrayList<PotionEffectType>();
-        tempEffects.add(PotionEffectType.REGENERATION);
-        tempEffects.add(PotionEffectType.POISON);
-        return tempEffects;
-    }
+	public static int getPotionEffectMaxAmp(PotionEffectType pet) {
+		if (pet == PotionEffectType.ABSORPTION) {
+			return 2;
+		} else if (pet == PotionEffectType.DAMAGE_RESISTANCE) {
+			return 2;
+		} else if (pet == PotionEffectType.FIRE_RESISTANCE) {
+			return 3;
+		} else if (pet == PotionEffectType.HEALTH_BOOST) {
+			return 10;
+		} else if (pet == PotionEffectType.INCREASE_DAMAGE) {
+			return 2;
+		} else if (pet == PotionEffectType.REGENERATION) {
+			return 3;
+		} else if (pet == PotionEffectType.INVISIBILITY) {
+			return 1;
+		} else if (pet == PotionEffectType.SPEED) {
+			return 4;
+		} else if (pet == PotionEffectType.POISON) {
+			return 6;
+		} else if (pet == PotionEffectType.SLOW) {
+			return 1;
+		} else if (pet == PotionEffectType.WEAKNESS) {
+			return 2;
+		} else if (pet == PotionEffectType.WITHER) {
+			return 2;
+		}
 
-    public static int getPotionEffectMaxAmp(PotionEffectType pet)
-    {
-        if (pet == PotionEffectType.ABSORPTION)
-        {
-            return 2;
-        }
-        else if (pet == PotionEffectType.DAMAGE_RESISTANCE)
-        {
-            return 3;
-        }
-        else if (pet == PotionEffectType.FIRE_RESISTANCE)
-        {
-            return 3;
-        }
-        else if (pet == PotionEffectType.HEALTH_BOOST)
-        {
-            return 4;
-        }
-        else if (pet == PotionEffectType.REGENERATION)
-        {
-            return 3;
-        }
-        else if (pet == PotionEffectType.INVISIBILITY)
-        {
-            return 1;
-        }
-        else if (pet == PotionEffectType.SPEED)
-        {
-            return 6;
-        }
-        else if (pet == PotionEffectType.POISON)
-        {
-            return 6;
-        }
-        else if (pet == PotionEffectType.SLOW)
-        {
-            return 2;
-        }
-        else if (pet == PotionEffectType.WEAKNESS)
-        {
-            return 2;
-        }
-        else if (pet == PotionEffectType.WITHER)
-        {
-            return 2;
-        }
+		return 1;
+	}
 
-        return 1;
-    }
+	public static int getPotionEffectMaxDuration(PotionEffectType pet) {
 
-    public static int getPotionEffectMaxDuration(PotionEffectType pet){
-    	
-        int dur = 30;
+		int dur = DAY;
 
-        if (pet == PotionEffectType.ABSORPTION)
-        {
-            dur = 60;
-        }
-        else if (pet == PotionEffectType.DAMAGE_RESISTANCE)
-        {
-            dur = 60;
-        }
-        else if (pet == PotionEffectType.FIRE_RESISTANCE)
-        {
-            dur = 300;
-        }
-        else if (pet == PotionEffectType.HEALTH_BOOST)
-        {
-            dur = 300;
-        }
-        else if (pet == PotionEffectType.REGENERATION)
-        {
-            dur = 30;
-        }
-        else if (pet == PotionEffectType.INVISIBILITY)
-        {
-            dur = 30;
-        }
-        else if (pet == PotionEffectType.SPEED)
-        {
-            dur = 60;
-        }
-        else if (pet == PotionEffectType.POISON)
-        {
-            dur = 60;
-        }
-        else if (pet == PotionEffectType.SLOW)
-        {
-            dur = 60;
-        }
-        else if (pet == PotionEffectType.WEAKNESS)
-        {
-            dur = 60;
-        }
-        else if (pet == PotionEffectType.WITHER)
-        {
-            dur = 20;
-        }
+		// if (pet == PotionEffectType.ABSORPTION) {
+		// dur = 60;
+		// } else if (pet == PotionEffectType.DAMAGE_RESISTANCE) {
+		// dur = 60;
+		// } else if (pet == PotionEffectType.INCREASE_DAMAGE) {
+		// dur = 30;
+		// } else if (pet == PotionEffectType.FIRE_RESISTANCE) {
+		// dur = 300;
+		// } else if (pet == PotionEffectType.HEALTH_BOOST) {
+		// dur = 300;
+		// } else if (pet == PotionEffectType.REGENERATION) {
+		// dur = 30;
+		// } else
+		if (pet == PotionEffectType.INVISIBILITY) {
+			dur = MINUTE;
+		}
+		// } else if (pet == PotionEffectType.SPEED) {
+		// dur = 60;
+		// } else if (pet == PotionEffectType.POISON) {
+		// dur = 60;
+		// } else if (pet == PotionEffectType.SLOW) {
+		// dur = 60;
+		// } else if (pet == PotionEffectType.WEAKNESS) {
+		// dur = 60;
+		// } else if (pet == PotionEffectType.WITHER) {
+		// dur = 20;
+		// }
 
-        return Functions.SecondsToTicks(dur);
-    }
+		return Functions.SecondsToTicks(dur);
+	}
 
-    public static PotionEffectType undeadReveseEffects(PotionEffectType pet)
-    {
-        if (pet == PotionEffectType.HEAL)
-        {
-            return PotionEffectType.HARM;
-        }
+	public static PotionEffectType undeadReveseEffects(PotionEffectType pet) {
+		if (pet == PotionEffectType.HEAL) {
+			return PotionEffectType.HARM;
+		}
 
-        return pet;
-    }
+		return pet;
+	}
 
-    public static List<PotionEffectType> negativeEffectsForMobs()
-    {
-        List<PotionEffectType> tempEffects = new ArrayList<PotionEffectType>();
-        tempEffects.add(PotionEffectType.POISON);
-        tempEffects.add(PotionEffectType.SLOW);
-        // tempEffects.add(PotionEffectType.HARM);
-        tempEffects.add(PotionEffectType.WEAKNESS);
-        // tempEffects.add(PotionEffectType.WITHER);
-        return tempEffects;
-    }
+	public static List<PotionEffectType> negativeEffectsForMobs() {
+		List<PotionEffectType> tempEffects = new ArrayList<PotionEffectType>();
+		tempEffects.add(PotionEffectType.POISON);
+		tempEffects.add(PotionEffectType.SLOW);
+		// tempEffects.add(PotionEffectType.HARM);
+		tempEffects.add(PotionEffectType.WEAKNESS);
+		// tempEffects.add(PotionEffectType.WITHER);
+		return tempEffects;
+	}
 
-    public static List<PotionEffectType> positiveEffectsForMobs()
-    {
-        List<PotionEffectType> tempEffects = new ArrayList<PotionEffectType>();
-        tempEffects.add(PotionEffectType.ABSORPTION);
-        tempEffects.add(PotionEffectType.DAMAGE_RESISTANCE);
-        tempEffects.add(PotionEffectType.FIRE_RESISTANCE);
-        // tempEffects.add(PotionEffectType.HEALTH_BOOST);
-        tempEffects.add(PotionEffectType.REGENERATION);
-        // tempEffects.add(PotionEffectType.HEAL);
-        tempEffects.add(PotionEffectType.INVISIBILITY);
-        tempEffects.add(PotionEffectType.SPEED);
-        return tempEffects;
-    }
+	public static List<PotionEffectType> positiveEffectsForMobs() {
+		List<PotionEffectType> tempEffects = new ArrayList<PotionEffectType>();
+		tempEffects.add(PotionEffectType.ABSORPTION);
+		tempEffects.add(PotionEffectType.DAMAGE_RESISTANCE);
+		// tempEffects.add(PotionEffectType.FIRE_RESISTANCE);
+		tempEffects.add(PotionEffectType.INCREASE_DAMAGE);
+		// tempEffects.add(PotionEffectType.HEALTH_BOOST);
+		// tempEffects.add(PotionEffectType.REGENERATION);
+		// tempEffects.add(PotionEffectType.HEAL);
+		// tempEffects.add(PotionEffectType.INVISIBILITY);
+		tempEffects.add(PotionEffectType.SPEED);
+		return tempEffects;
+	}
 
-    public static List<PotionEffectType> positiveEffects()
-    {
-        List<PotionEffectType> tempEffects = new ArrayList<PotionEffectType>();
-        tempEffects.add(PotionEffectType.ABSORPTION);
-        tempEffects.add(PotionEffectType.DAMAGE_RESISTANCE);
-        tempEffects.add(PotionEffectType.FAST_DIGGING);
-        tempEffects.add(PotionEffectType.FIRE_RESISTANCE);
-        tempEffects.add(PotionEffectType.HEAL);
-        tempEffects.add(PotionEffectType.HEALTH_BOOST);
-        tempEffects.add(PotionEffectType.INVISIBILITY);
-        tempEffects.add(PotionEffectType.JUMP);
-        tempEffects.add(PotionEffectType.NIGHT_VISION);
-        tempEffects.add(PotionEffectType.REGENERATION);
-        tempEffects.add(PotionEffectType.SPEED);
-        tempEffects.add(PotionEffectType.WATER_BREATHING);
-        return tempEffects;
-    }
+	public static List<PotionEffectType> positiveEffects() {
+		List<PotionEffectType> tempEffects = new ArrayList<PotionEffectType>();
+		tempEffects.add(PotionEffectType.ABSORPTION);
+		tempEffects.add(PotionEffectType.DAMAGE_RESISTANCE);
+		tempEffects.add(PotionEffectType.FAST_DIGGING);
+		tempEffects.add(PotionEffectType.FIRE_RESISTANCE);
+		tempEffects.add(PotionEffectType.HEAL);
+		tempEffects.add(PotionEffectType.HEALTH_BOOST);
+		tempEffects.add(PotionEffectType.INVISIBILITY);
+		tempEffects.add(PotionEffectType.JUMP);
+		tempEffects.add(PotionEffectType.NIGHT_VISION);
+		tempEffects.add(PotionEffectType.REGENERATION);
+		tempEffects.add(PotionEffectType.SPEED);
+		tempEffects.add(PotionEffectType.WATER_BREATHING);
+		return tempEffects;
+	}
 
 }
