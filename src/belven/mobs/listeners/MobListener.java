ff785e34f11794996da7662f7571cd80444989ec
belven <belven000@gmail.com>
2014-11-20 23:51:51 +0000

    Re-worked damage and defense

--- src/belven/mobs/listeners/MobListener.java
+++ src/belven/mobs/listeners/MobListener.java
@@ -3,6 +3,7 @@ package belven.mobs.listeners;
 import java.util.List;
 import java.util.Random;
 
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
@@ -10,11 +11,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import resources.EntityFunctions;
-import resources.Functions;
 import belven.mobs.MobBuffManager;
 import belven.mobs.resources.functions;
 
@@ -31,21 +30,26 @@ public class MobListener implements Listener {
 	public void onCreatureSpawnEvent(CreatureSpawnEvent event) {
 
 		if (EntityFunctions.IsAMob(event.getEntityType())) {
-			LivingEntity le = event.getEntity();
-
-			PotionEffectType positiveEffect = getPositiveEffect(le);
-			int posAmplifier = functions.getPotionEffectMaxAmp(positiveEffect);
-			int posDuration = functions.getPotionEffectMaxDuration(positiveEffect);
-
-			PotionEffectType negativeEffect = getNegativeEffect(le);
-			int negAmplifier = functions.getPotionEffectMaxAmp(positiveEffect);
-			int negDuration = functions.getPotionEffectMaxDuration(positiveEffect);
-
-			PotionEffect positive = new PotionEffect(positiveEffect, posDuration, posAmplifier);
-
-			new PotionEffect(negativeEffect, negDuration, negAmplifier);
-
-			le.addPotionEffect(positive, true);
+			// LivingEntity le = event.getEntity();
+			//
+			// PotionEffectType positiveEffect = getPositiveEffect(le);
+			// int posAmplifier =
+			// functions.getPotionEffectMaxAmp(positiveEffect);
+			// int posDuration =
+			// functions.getPotionEffectMaxDuration(positiveEffect);
+			//
+			// PotionEffectType negativeEffect = getNegativeEffect(le);
+			// int negAmplifier =
+			// functions.getPotionEffectMaxAmp(positiveEffect);
+			// int negDuration =
+			// functions.getPotionEffectMaxDuration(positiveEffect);
+			//
+			// PotionEffect positive = new PotionEffect(positiveEffect,
+			// posDuration, posAmplifier);
+			//
+			// new PotionEffect(negativeEffect, negDuration, negAmplifier);
+			//
+			// le.addPotionEffect(positive, true);
 			// le.addPotionEffect(negative, true);
 		}
 	}
@@ -63,7 +67,7 @@ public class MobListener implements Listener {
 		if (EntityFunctions.IsAMob(damagedEntity.getType())) {
 			damageEntity.setNoDamageTicks(5);
 		} else {
-			damageEntity.setNoDamageTicks(Functions.SecondsToTicks(1));
+			damageEntity.setNoDamageTicks(20);
 		}
 
 		damager.getEquipment().getItemInHand()
@@ -91,33 +95,45 @@ public class MobListener implements Listener {
 	public double getArmourGadeValue(ItemStack is) {
 		String grade = is.getType().toString();
 		if (grade.contains("LEATHER")) {
-			return 0.075;
+			return 0.007 + getEnchantmentBonus(is);
 		} else if (grade.contains("IRON")) {
-			return 0.01;
+			return 0.010 + getEnchantmentBonus(is);
 		} else if (grade.contains("GOLD")) {
-			return 0.075;
+			return 0.007 + getEnchantmentBonus(is);
 		} else if (grade.contains("DIAMOND")) {
-			return 0.02;
+			return 0.020 + getEnchantmentBonus(is);
 		}
 
-		return 0.005;
+		return 0;
 	}
 
 	private double getWeaponBonus(ItemStack itemInHand) {
+
 		switch (itemInHand.getType()) {
 		case WOOD_SWORD:
-			return 0.10;
+			return 0.075 + getEnchantmentBonus(itemInHand);
 		case STONE_SWORD:
-			return 0.15;
+			return 0.100 + getEnchantmentBonus(itemInHand);
 		case GOLD_SWORD:
-			return 0.15;
+			return 0.125 + getEnchantmentBonus(itemInHand);
 		case IRON_SWORD:
-			return 0.20;
+			return 0.150 + getEnchantmentBonus(itemInHand);
 		case DIAMOND_SWORD:
-			return 0.025;
+			return 0.200 + getEnchantmentBonus(itemInHand);
 		default:
-			return 0.1;
+			return 0;
 		}
+	}
+
+	private double getEnchantmentBonus(ItemStack is) {
+		if (is.containsEnchantment(Enchantment.ARROW_DAMAGE)) {
+			return is.getEnchantmentLevel(Enchantment.ARROW_DAMAGE);
+		} else if (is.containsEnchantment(Enchantment.DAMAGE_ALL)) {
+			return is.getEnchantmentLevel(Enchantment.DAMAGE_ALL);
+		} else if (is.containsEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL)) {
+			return is.getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL);
+		}
+		return 0;
 	}
 
 	private PotionEffectType getPositiveEffect(LivingEntity le) {
